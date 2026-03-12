f5f07b86d3708203cbec9f56a10b77cd9638c9a5
Alking <alrey2635@gmail.com>
2026-03-12 20:53:54 +0300

    added openapi

--- src/main/java/com/alpha/td_spring_boot_api/TdSpringBootApiApplication.java
+++ src/main/java/com/alpha/td_spring_boot_api/TdSpringBootApiApplication.java
@@ -2,8 +2,12 @@ package com.alpha.td_spring_boot_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Slf4j
 @SpringBootApplication
+
 public class TdSpringBootApiApplication {
 
     public static void main(String[] args) {
