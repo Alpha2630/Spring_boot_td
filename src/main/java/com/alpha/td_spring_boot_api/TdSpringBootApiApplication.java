99d43688dc99d7d2830179e3d1be8341b694fa70
Alking <alrey2635@gmail.com>
2026-03-12 19:20:14 +0300

    first commit

---
+++ src/main/java/com/alpha/td_spring_boot_api/TdSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.td_spring_boot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdSpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdSpringBootApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpha/td_spring_boot_api/TdSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.td_spring_boot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdSpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
